52dd553697f93d2b66bcd5b500714fbadbb3e012
Dannyama <amadansoa45@gmail.com>
2022-10-19 16:25:39 -0700

    done

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,24 +1,21 @@
 #include "main.h"
+
 /**
- * print_sign - function to check for a sign of a number
- * @n:  is the int that will use for the argument of the function
- * Return: 0
+ * _isalpha.c - main function
+ *
+ * @c: chareacter to campare 
+ *
+ * Return: 1 if true, 0 if false
+ *
  */
-int print_sign(int n)
+
+int _isalpha(int c);
 {
-	if (n > 0)
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
-	_putchar('+');
-	return (1);
-}
-	else if (n < 0)
-	{
-	_putchar('-');
-	return (-1);
+		return(1);
+
 	}
-	else
-	{
-	_putchar('0');
-	return (0);
-	}
+	return(0);
 }
+ 
